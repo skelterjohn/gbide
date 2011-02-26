f5bc185448685ed98956c30c5379e12a628bfd57
John Asmuth <jasmuth@gmail.com>
2011-02-26 00:36:58 -0500

    new js

--- html/editor.js
+++ html/editor.js
@@ -1,22 +1,25 @@
 
-function Save()
+function save()
 {
-	var xmlhttp;
-	if (window.XMLHttpRequest)
-	{// code for IE7+, Firefox, Chrome, Opera, Safari
-		xmlhttp=new XMLHttpRequest();
-	}
-	else
-	{// code for IE6, IE5
-		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-	}
-	xmlhttp.onreadystatechange=function()
-	{
-		if (xmlhttp.readyState==4 && xmlhttp.status==200)
-		{
-			document.getElementById("editfield").innerHTML=xmlhttp.responseText;
+	sendRequest("http://localhost:9090/save", 0, "stuff")
+}
+
+function sendRequest(url,callback,postData) {
+	var req = createXMLHTTPObject();
+	if (!req) return;
+	var method = (postData) ? "POST" : "GET";
+	req.open(method,url,true);
+	req.setRequestHeader('User-Agent','XMLHTTP/1.0');
+	if (postData)
+		req.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+	req.onreadystatechange = function () {
+		if (req.readyState != 4) return;
+		if (req.status != 200 && req.status != 304) {
+			//			alert('HTTP error ' + req.status);
+			return;
 		}
+		callback(req);
 	}
-	xmlhttp.open("GET","ajax_info.txt",true);
-	xmlhttp.send();
+	if (req.readyState == 4) return;
+	req.send(postData);
 }
