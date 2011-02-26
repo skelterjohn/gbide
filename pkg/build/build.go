618bab27ced26c1119a7cd89965b12c6b51acedc
John Asmuth <jasmuth@gmail.com>
2011-02-26 13:49:13 -0500

    added browse splash

---
+++ pkg/build/build.go
@@ -0,0 +1,29 @@
+package build
+
+import (
+	"os"
+	"template"
+	"bytes"
+)
+
+const BuildTemplatePath = "templates/build.template"
+
+var BuildT *template.Template
+
+func init() {
+	BuildT = template.New(nil)
+	BuildT.SetDelims("{{", "}}")
+	BuildT.ParseFile(BuildTemplatePath)
+}
+
+func GetBar() (code string) {
+	var err os.Error
+	if err == nil {
+		buf := bytes.NewBuffer([]byte{})
+		BuildT.Execute(buf, nil)
+		code = buf.String()
+	} else {
+		code = err.String()
+	}
+	return
+}
