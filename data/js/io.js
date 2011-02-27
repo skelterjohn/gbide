e2e6547f40297e6e03cd2b16522cd1e6cdf1c774
John Asmuth <jasmuth@gmail.com>
2011-02-27 16:21:53 -0500

    open/save buttons work, but they aren't layed out very well

---
+++ data/js/io.js
@@ -0,0 +1,29 @@
+
+var SaveContents = function(filename) {
+	data = aceEditor.getSession().getValue();
+	$.ajax(
+		   {
+		   type: "POST",
+		   url: "save",
+		   data: {filename:filename, data:data},
+		   context: document.body,
+		   success: function(data, textStatus, jqXHR){
+		   if (data != null) {
+		   alert(data);
+		   }
+		   }
+		   });
+}
+
+var LoadContents = function(filename) {
+	$.ajax(
+		   {
+		   type: "POST",
+		   url: "load",
+		   data: {filename:"README2"},
+		   context: document.body,
+		   success: function(data, textStatus, jqXHR){
+		   aceEditor.getSession().setValue(data)
+		   }
+		   });
+}
--- pkg/editor/save.go
+++ pkg/editor/save.go
@@ -7,13 +7,41 @@ import (
 
 func SaveHandler(ctx *web.Context) (code string) {
 	params := ctx.Request.Params
-	file := params["file"]
+	file := params["filename"]
 	data := params["data"]
 
 	fout, err := os.Open(file, os.O_CREATE|os.O_RDWR, 0644)
 	if err == nil {
 		fout.WriteString(data)
 		fout.Close()
+		code = "ok"
 	}
 	return
+}
+
+func LoadHandler(ctx *web.Context) {
+	params := ctx.Request.Params
+	filename := params["filename"]
+	
+	var fin *os.File
+	var err os.Error
+	fin, err = os.Open(filename, os.O_RDONLY, 0)
+	if err != nil {
+		ctx.WriteString(err.String())
+		return
+	}
+	buf := make([]byte, 1024)
+	for {
+		var n int
+		n, err = fin.Read(buf)
+		if err != nil {
+			//ctx.WriteString(err.String())
+			return
+		}
+		if n == 0 || err != nil {
+			break
+		}
+		ctx.Write(buf[0:n])
+	}
+	fin.Close()
 }
