20b1746f1d960350da0cb6cbab728a3578621ed1
John Asmuth <jasmuth@gmail.com>
2011-03-04 19:23:12 -0500

    added jscroll stuff, not working

--- data/js/fs.js
+++ data/js/fs.js
@@ -1,32 +1,37 @@
-$(document).ready(function(){
+$(document).ready(function(){LoadFileBrowser()});
+
+var LoadFileBrowser = function(){
+	
+	UrlFinder = function(node) {
+		if (node == -1) {
+			return "/ls/"
+		} else {
+			return "/ls/"+node.attr("id")
+		}
+	}
 	
 	$("#filebrowser").jstree({ 
 		"xml_data" : {
 			"ajax" : {
-				"url" : function(node) {
-					if (node == -1) {
-						return "/ls/"
-					} else {
-						return "/ls/"+node.attr("id")
-					}
-				},
-							
+				"url" : UrlFinder,
 			},
 		},
-							
+
 		"ui" : {
 			"select_limit" : 1,
 			"selected_parent_close" : "select_parent",
 		},
-							 
+
 		"plugins" : [ "themes", "xml_data", "ui" ]
 	});	  
 	$("#filebrowser").jstree("set_theme","apple");
 	$("#filebrowser").jstree("toggle_icons")
 	$("#filebrowser").jstree("toggle_dots")
-				  
-	$("#filebrowser").delegate('a', 'click', ClickFileTree); 
-});
+	
+	$("#filebrowser").delegate('a', 'click', ClickFileTree) 
+	
+	$("#filescroll").jScrollPane()
+}
 
 var ClickFileTree = function(e) {
 	n = $.jstree._focused()._get_node(this)
